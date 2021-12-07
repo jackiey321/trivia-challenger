f4bbe25eab6e21899b1350abe7e338f5fd9ca28d
Jaclyn Yang <jackiey321@gmail.com>
2021-12-07 00:06:55 -0500

    changed how to access router

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index').router;
 var usersRouter = require('./routes/users');
 
 //Import the mongoose module
